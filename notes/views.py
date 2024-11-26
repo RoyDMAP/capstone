42b8de11d0e342cf390d1f9da6e7439da5c4c3ea
RoyDMAP <roy.dimapilis@sdgku.edu>
2024-11-25 19:45:35 -0800

    FSDI 118 assignment 5 capstone

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView 
-from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic import ListView, DetailView
 from django.urls import reverse
 from .forms import NoteForm
 from .models import Note
@@ -22,10 +22,28 @@ class CreateNoteView(CreateView):
     template_name="notes/create_note.html"
     form_class = NoteForm
 
-
     def get_success_url(self):
         return reverse('list_notes')
     
 class ListNotesView(ListView):
     template_name = "notes/list_note.html"
     model = Note
+
+class DetailNoteView(DetailView):
+    template_name = "notes/detail_note.html"
+    model = Note
+
+class DeleteNoteView(DeleteView):
+    template_name = "notes/delete_note.html"
+    model = Note
+
+    def get_success_url(self):
+        return reverse("list_notes")
+    
+class UpdateNoteView(UpdateView):
+    template_name = "notes/update_note.html"
+    model = Note
+    form_class = NoteForm
+
+    def get_success_url(self):
+        return reverse("list_notes")
