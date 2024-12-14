e4088d012607d79ed076b8fed5e96bde624a5ed3
RoyDMAP <roy.dimapilis@sdgku.edu>
2024-12-14 14:48:24 -0800

    FSDI 120 class 1 CAPSTONE

--- config/settings.py
+++ config/settings.py
@@ -25,13 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f9fok74qltv)!pkejrx#o3-2^r^&2+x@g(%zvpj_w(r*k_25@k'
+SECRET_KEY = env("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["unifyd-c5fdfdb7526c.herokuapp.com"]
+ALLOWED_HOSTS = ["unifyd-c5fdfdb7526c.herokuapp.com", '127.0.0.1']
 
 
 # Application definition
