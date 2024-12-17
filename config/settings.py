adc801a2e8204ca8db5a1582209ae4f0f42c7db9
RoyDMAP <roy.dimapilis@sdgku.edu>
2024-12-16 19:37:03 -0800

    first deployment

--- config/settings.py
+++ config/settings.py
@@ -171,6 +171,7 @@ MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/"
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = "public-read"
 
+
 STORAGES = { 
     "default": {
         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
